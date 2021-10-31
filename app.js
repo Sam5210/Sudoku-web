83b7a06118a78baaa6992c4ecae47cf3942f6e79
apple <samuelyang5210@gmail.com>
2021-10-31 19:21:53 +0800

    Set up menu and game

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/home.html");
+    response.render("menu");
+});
+app.post("/game", (request, response) => {
+    console.log(Object.keys(request.body));
+    console.log("\n\n\n");
+    response.send("<h1 class='h1 text-center mt-4'>Your response was sent !</h1>")
 });
 app.listen(3000);
